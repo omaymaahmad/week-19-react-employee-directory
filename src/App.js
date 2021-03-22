9409d280595316029b702646fab5017303d5f75c
omayma ahmad <omayma.ahmad25@gmail.com>
2021-03-22 18:54:06 +0000

    feat: set up main class component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "./components/Header";
+import Main from "./components/Main";
 
 import './App.css';
 
@@ -7,6 +8,7 @@ function App() {
   return (
    <div>
      <Header />
+     <Main />
    </div>
   );
 }
---
+++ src/components/EmployeeData.js
@@ -0,0 +1,20 @@
+import React, {Component} from "react";
+
+
+export default class EmployeeData extends Component {
+    state = {
+        employees: [{}]
+    }
+
+    componentDidMount(){
+
+    }
+
+    render(){
+        return (
+            <>
+
+            </>
+        )
+    }
+}
